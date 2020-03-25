94b7c91d1fff743eb30617a75e15a599bc738b2d
Nathan Lin <nathanlukelin@gmail.com>
2020-03-24 18:41:08 -0700

    Update SimpleTest.java

--- src/test/java/com/example/project/SimpleTest.java
+++ src/test/java/com/example/project/SimpleTest.java
@@ -1,5 +1,16 @@
 public class SimpleTest {
 	
+	package com.example.project;
+
+	import static org.junit.jupiter.api.Assertions.assertEquals;
+
+	import org.junit.jupiter.api.DisplayName;
+	import org.junit.jupiter.api.Test;
+	import org.junit.jupiter.params.ParameterizedTest;
+	import org.junit.jupiter.params.provider.CsvSource;
+
+	import java.io.*;
+	
 	@Test
 	public static void main(String[] args)
 	{
